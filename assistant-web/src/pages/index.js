c2f5620463fc024432789e06290137f9624bf4ac
Harry Kodden <harry@e-tunity.nl>
2023-04-17 12:01:54 +0200

    wip

--- assistant-web/src/pages/index.js
+++ assistant-web/src/pages/index.js
@@ -115,60 +115,66 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col h-screen">
-      {/* Navbar */}
-      <nav className="shadow px-4 py-2 flex flex-row justify-between items-center">
-        <div className="text-xl font-bold">Assistant</div>
+    <>
+      <Head>
+        <title>Assistant</title>
+      </Head>
+
+      <div className="flex flex-col h-screen">
+        {/* Navbar */}
+        <nav className="shadow px-4 py-2 flex flex-row justify-between items-center">
+          <div className="text-xl font-bold">Assistant</div>
+          <div>
+            <input
+              type="password"
+              className="border p-1 rounded"
+              onChange={(e) => setApiKey(e.target.value)}
+              value={apiKey}
+              placeholder="Paste API Key here"
+            />
+          </div>
+        </nav>
+
+        {/* Message History */}
+        <div className="flex-1 overflow-y-scroll ">
+          <div className="w-full max-w-screen-md mx-auto px-4">
+            {messages
+              .filter((message) => message.role !== "system")
+              .map((message, idx) => (
+                <div key={idx} className="my-3">
+                  <div className="font-bold">
+                    {message.role === "user" ? "You" : "Assistant"}
+                  </div>
+                  <div className="text-lg prose">
+                    <ReactMarkdown>{message.content}</ReactMarkdown>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+
+        {/* Message Input Box */}
         <div>
-          <input
-            type="password"
-            className="border p-1 rounded"
-            onChange={(e) => setApiKey(e.target.value)}
-            value={apiKey}
-            placeholder="Paste API Key here"
-          />
-        </div>
-      </nav>
-
-      {/* Message History */}
-      <div className="flex-1 overflow-y-scroll ">
-        <div className="w-full max-w-screen-md mx-auto px-4">
-          {messages
-            .filter((message) => message.role !== "system")
-            .map((message, idx) => (
-              <div key={idx} className="my-3">
-                <div className="font-bold">
-                  {message.role === "user" ? "You" : "Assistant"}
-                </div>
-                <div className="text-lg prose">
-                  <ReactMarkdown>{message.content}</ReactMarkdown>
-                </div>
-              </div>
-            ))}
+          <div className="w-full max-w-screen-md mx-auto flex px-4 pb-4 items-start">
+            <TextareaAutosize
+              value={userMessage}
+              autoFocus
+              maxRows={10}
+              onKeyDown={handleKeyDown}
+              placeholder="Ask me anything.."
+              onChange={(e) => setUserMessage(e.target.value)}
+              className="border text-lg rounded-md p-2 flex-1 resize-none"
+              rows={1}
+            />
+            <button
+              onClick={sendRequest}
+              className="bg-blue-500 hover:bg-blue-600 border rounded-md text-white text-lg w-20 p-2 ml-2"
+            >
+              Send
+            </button>
+          </div>
         </div>
       </div>
-
-      {/* Message Input Box */}
-      <div>
-        <div className="w-full max-w-screen-md mx-auto flex px-4 pb-4 items-start">
-          <TextareaAutosize
-            value={userMessage}
-            autoFocus
-            maxRows={10}
-            onKeyDown={handleKeyDown}
-            placeholder="Ask me anything.."
-            onChange={(e) => setUserMessage(e.target.value)}
-            className="border text-lg rounded-md p-2 flex-1 resize-none"
-            rows={1}
-          />
-          <button
-            onClick={sendRequest}
-            className="bg-blue-500 hover:bg-blue-600 border rounded-md text-white text-lg w-20 p-2 ml-2"
-          >
-            Send
-          </button>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
