594287d784f5c2dc6ae449843d6bde80ba55fbc3
Harry Kodden <harry@e-tunity.nl>
2023-04-17 07:57:48 +0000

    wip

--- assistant-web/src/pages/index.js
+++ assistant-web/src/pages/index.js
@@ -1,10 +1,25 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useState } from "react";
 
 export default function Home() {
+
+  const [apiKey, setApiKey] = useState("");
+  
   return (
-    <div>Hello world</div>
-  )
+    <div className="flex flex-col h-screen">
+      /* Navbar */
+      <nav className="bg-white shadow w-full">
+          <div className="px-4 h-14 flex justify-between itens-center">
+            <div>
+              <input
+                type="password"
+                className="border rounded p-1"
+                placeholder="Enter API key..."
+                value={apiKey}
+                onchange={(e) => setApiKey(e.target.value)}
+               />
+              </div>
+          </div>
+        </nav>
+    </div>
+  );
 }
